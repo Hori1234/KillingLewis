560b6aac54c557724dae9ff0048ea7d85266ed8d
popacosminmarian <popacosminmarian@gmail.com>
2019-05-19 05:00:08 +0300

    Fixed small error where some mouse click events would not register

--- src/killinglewis/input/MouseInput.java
+++ src/killinglewis/input/MouseInput.java
@@ -2,6 +2,7 @@ package killinglewis.input;
 
 import org.lwjgl.glfw.GLFWMouseButtonCallback;
 
+import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
 import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
 
 public class MouseInput extends GLFWMouseButtonCallback {
@@ -12,6 +13,6 @@ public class MouseInput extends GLFWMouseButtonCallback {
     @Override
     public void invoke(long window, int button, int action, int mods) {
         // if the button has not been released, set its corresponding boolean value to true; otherwise set to false
-        mouseButton[button] = (action != GLFW_RELEASE);
+        mouseButton[button] = action == GLFW_PRESS;
     }
 }
