cb45290265caadb904d3ccf2f6793f3b0d27c36c
Illustriouss <jorishb@hotmail.com>
2019-06-13 14:23:55 +0200

    fix placing obstruction on lewis crashed game

--- src/killinglewis/Models/Terrain.java
+++ src/killinglewis/Models/Terrain.java
@@ -44,9 +44,12 @@ public class Terrain {
         int mazeX = (int) floor((x / WINDOW_WIDTH) / cellWidth);
         int mazeY = (int) floor((y / WINDOW_HEIGHT) / cellHeight);
 
-        maze.setWall(mazeX, mazeY, lewisX, lewisY);
-
-        walls = getWalls();
+        if (mazeX == lewisY && mazeY == lewisX) { // maze X,Y and lewis X,Y are flipped
+            System.out.println("Cannot place obstruction on Lewis");
+        } else {
+            maze.setWall(mazeX, mazeY, lewisX, lewisY);
+            walls = getWalls();
+        }
     }
 
     public Vector3f getCellPosition(int x, int y) {
