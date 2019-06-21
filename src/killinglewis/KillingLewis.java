8ffc8058d7636c1f32961bf9c1151e14a39ed15d
popacosminmarian <popacosminmarian@gmail.com>
2019-06-21 20:08:39 +0200

    fixed screen size

--- src/killinglewis/KillingLewis.java
+++ src/killinglewis/KillingLewis.java
@@ -79,6 +79,7 @@ public class KillingLewis implements Runnable {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
         // create the window if it does not exist yet
         if (window == NULL || NEW_WINDOW_ON_RESTART) {
@@ -202,13 +203,18 @@ public class KillingLewis implements Runnable {
     }
 
     private void render() {
-        glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+        int[] x = new int[1], y = new int[1];
+        glfwGetFramebufferSize(window, x, y);
+        glViewport(0, 0, x[0], y[0]);
         glEnable(GL_DEPTH_TEST);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         level.render();
         ParticleManager.renderParticles();
         //shadowMaker.render(level,light);
+
         glfwSwapBuffers(window);
+        glfwGetCurrentContext();
+        glfwMakeContextCurrent(window);
     }
 
     /**
